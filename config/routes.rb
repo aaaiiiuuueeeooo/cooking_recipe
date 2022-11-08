155af5d07ae2b43dabd5eeaf568fecde799cb9b7
EC2 Default User <107116870+aaaiiiuuueeeooo@users.noreply.github.com>
2022-11-08 14:14:01 +0000

    cooking_recipe4

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,17 @@ end
 scope module: :public do
 root to: 'homes#top'
   get 'homes/top'
+  resources :customers
+  resources :homes
+  resources :recipes
+  
 end   
+  namespace :admin do
+    get 'homes/top' => 'homes#top'
+    resources :customers
+    
+    resources :recipes
+    
+end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
