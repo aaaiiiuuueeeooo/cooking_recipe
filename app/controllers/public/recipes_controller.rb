486519f54a7059dd9cc218fb9b0a29c46168e933
EC2 Default User <107116870+aaaiiiuuueeeooo@users.noreply.github.com>
2022-11-09 14:42:42 +0000

    cooking_recipe5

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -1,2 +1,22 @@
 class Public::RecipesController < ApplicationController
+    def index
+        @recipe = Recipe.new
+        @recipes = Recipe.all
+    end
+
+    def new
+        @recipe = Recipe.new
+    end
+
+    def create
+        @recipe = Recipe.new(recipe_params)
+        @recipe.save
+        redirect_to recipes_path
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:image,:recipe_name,:introduction)
+    end
 end
